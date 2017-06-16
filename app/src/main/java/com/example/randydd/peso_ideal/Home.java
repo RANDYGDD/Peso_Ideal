2efa4021657690e59156923198778f8711f00a38
RANDYGDD <randydilone@outlook.com>
2017-06-15 22:51:25 -0400

    Jueves Corpus Christias

---
+++ app/src/main/java/com/example/randydd/peso_ideal/Home.java
@@ -0,0 +1,43 @@
+package com.example.randydd.peso_ideal;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        findViewById(R.id.calcular).setOnClickListener(this);
+        findViewById(R.id.informacion).setOnClickListener(this);
+        findViewById(R.id.acerca_de).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.calcular:
+                Intent calcular=new Intent(Home.this,MainActivity.class);
+                startActivity(calcular);
+                break;
+            case R.id.informacion:
+                Intent informacion=new Intent(Home.this,informacion.class);
+                startActivity(informacion);
+                break;
+
+            case R.id.acerca_de:
+                Intent acerca_de=new Intent(Home.this,acerca_de.class);
+                startActivity(acerca_de);
+                break;
+        }
+
+
+
+    }
+}
